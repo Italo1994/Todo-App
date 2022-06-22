d37c48e00a0a35864d501be4ece413ab04cdff03
Italo1994 <italohortiz2015@gmail.com>
2022-06-22 01:43:05 -0300

    Criando a função para remover itens da lista

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,20 +4,8 @@ var img_header = document.querySelector("#header");
 var main = document.querySelector('#main');
 var footer = document.querySelector("#footer");
 var COUNT_CLICKS = 0;
-
-
-var todo_text = document.getElementById("todo-text");
-var button_add = document.getElementById("button-add");
-
-var list = document.getElementById("list");
-list.style.display = 'grid';
-list.style.gridTemplateColumns = '1fr 9fr';
-list.style.justifyContent = 'center';
-list.style.alignItems = 'center';
-
-
-var item = document.createElement("li");
-var text = document.createTextNode(todo_text.value);
+var QUANT_ICONS = 0;
+var count = 0;
 
 
 function alter_bg() {
@@ -25,49 +13,70 @@ function alter_bg() {
     if(COUNT_CLICKS % 2 == 0) {
         img_icon.setAttribute('src', '../../images/icon-moon.svg');
         img_header.style.background = 'url("../../images/bg-desktop-light.jpg")';
-
         main.style.backgroundColor = '#FFF';
         footer.style.backgroundColor = '#FFF';
         container.style.backgroundColor = '#FFF';
-
-        list.style.backgroundColor = '#FFF';
-        item.style.backgroundColor = '#FFF';
 
         img_header.style.transition = 'all .5s';
     }
     else {
         img_icon.setAttribute('src', '../../images/icon-sun.svg');
         img_header.style.background = 'url("../../images/bg-desktop-dark.jpg")';
-
         main.style.backgroundColor = '#181824';
         footer.style.backgroundColor = "#181824";
         container.style.backgroundColor = '#181824';
-
-        list.style.backgroundColor = '#25273C';
-        item.style.backgroundColor = '#25273C';
-        item.style.color = '#FFF';
         
         img_header.style.transition = 'all .5s';
     }
 }
 
+var todo_text = document.getElementById("todo-text");
+var button_add = document.getElementById("button-add");
+
+var list = document.getElementById("list");
+var inputCheckbox = [];
+var item = [];
+var deleteIcone = [];
+var text_delete = [];
 
 function addTodo() {
     if(todo_text.value.length < 1) {
         return 0;
     }
 
-    var inputCheckbox = document.createElement("input");
-    inputCheckbox.setAttribute('type', 'checkbox');
-    inputCheckbox.style.width = '100%';
-    inputCheckbox.style.height = '100%';
-    inputCheckbox.style.marginTop = '0px';
-    inputCheckbox.style.borderRadius = '10px';
+    inputCheckbox[count] = document.createElement("input");
+    inputCheckbox[count].setAttribute('type', 'checkbox');
+    inputCheckbox[count].setAttribute('id', count);
+    inputCheckbox[count].style.width = '100%';
+    inputCheckbox[count].style.height = '100%';
 
+    item[count] = document.createElement("span");
+    item[count].setAttribute('id', count);
+    item[count].style.width = '100%';
+    item[count].setAttribute('id', QUANT_ICONS+1);
+
+    deleteIcone[count] = document.createElement("i");
+    deleteIcone[count].setAttribute('class', 'fa-solid fa-trash');
+    deleteIcone[count].setAttribute('id', count);
+    // deleteIcone[count].innerHTML = count + 1;
+    deleteIcone[count].setAttribute('onclick', 'deleteIcon(this.id)');
+    deleteIcone[count].style.cursor = 'pointer';
     
-    item.appendChild(text);
-    list.appendChild(inputCheckbox);
-    list.appendChild(item);
+
+    var text = document.createTextNode(todo_text.value);
+
+    item[count].appendChild(text);
+    item[count].appendChild(deleteIcone[count]);
+    list.appendChild(inputCheckbox[count]);
+    list.appendChild(item[count]);
 
     todo_text.value = ""; // limpando o texto do input ao adicionar
+    QUANT_ICONS++;
+    count++;
+}
+
+function deleteIcon(e) {
+    inputCheckbox[e].style.display = 'none';
+    item[e].style.display = 'none';
+    deleteIcone[e].style.display = 'none';
 }
